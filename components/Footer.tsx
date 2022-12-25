9d12362c1aec3b772597d0bbd85f4a00e2c49191
Nikhil Lama <nikhil.lama@ekbana.info>
2022-12-25 20:22:36 +0545

    feat: footer design

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,11 @@
+import { Typography, Box } from '@mui/material';
 import React from 'react';
 
 const Footer = () => {
 	return (
-		<div>
-			<h3>Footer</h3>
-		</div>
+		<Box component='footer' sx={{ marginTop: 35 }} textAlign='center'>
+			<Typography variant='body2'>Created by Nikhil Lama.</Typography>
+		</Box>
 	);
 };
 
