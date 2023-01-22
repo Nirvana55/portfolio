7192c379d3f7affa49356f7b32fb98975afced4c
Nikhil Lama <nikhil.lama@ekbana.info>
2023-01-22 19:32:23 +0545

    feat: add background color as a global color

--- config/theme.ts
+++ config/theme.ts
@@ -3,6 +3,10 @@ import { createTheme } from '@mui/material';
 export const theme = createTheme({
 	palette: {
 		mode: 'dark',
+		background: {
+			default: '#1E2022',
+			paper: '#25282A',
+		},
 	},
 	typography: {
 		fontFamily: "'Montserrat', sans-serif;",
