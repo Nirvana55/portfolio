c33e94593b761ea4a8a245b4455a414b40da6cad
Nirvana55 <lamabruno55@gmail.com>
2023-11-11 19:38:43 +0545

    fix: prod build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,32 +1,32 @@
-import { Box, createTheme, CssBaseline, ThemeProvider } from '@mui/material';
-import { AppProps } from 'next/app';
-import { useMemo, useState } from 'react';
-import Footer from '../components/Footer';
-import Layout from '../components/Layout';
-import NavBar from '../components/Navbar';
-import getTheme from '../config';
-import '../styles/globals.css';
+import { Box, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import { AppProps } from "next/app";
+import { useMemo, useState } from "react";
+import Footer from "../components/Footer";
+import Layout from "../components/Layout";
+import NavBar from "../components/Navbar";
+import getTheme from "../config-theme";
+import "../styles/globals.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-	const [isDarkMode, setIsDarkMode] = useState(true);
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
-	const selectTheme = useMemo(() => {
-		let theme = createTheme(getTheme(isDarkMode ? 'dark' : 'light'));
-		return theme;
-	}, [isDarkMode]);
+  const selectTheme = useMemo(() => {
+    let theme = createTheme(getTheme(isDarkMode ? "dark" : "light"));
+    return theme;
+  }, [isDarkMode]);
 
-	return (
-		<>
-			<ThemeProvider theme={selectTheme}>
-				<CssBaseline />
-				<Layout>
-					<NavBar />
-					<Box sx={{ pl: 4, pr: 4 }}>
-						<Component {...pageProps} />
-					</Box>
-					<Footer />
-				</Layout>
-			</ThemeProvider>
-		</>
-	);
+  return (
+    <>
+      <ThemeProvider theme={selectTheme}>
+        <CssBaseline />
+        <Layout>
+          <NavBar />
+          <Box sx={{ pl: 4, pr: 4 }}>
+            <Component {...pageProps} />
+          </Box>
+          <Footer />
+        </Layout>
+      </ThemeProvider>
+    </>
+  );
 }
