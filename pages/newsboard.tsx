b2ffe65e4191c2fadc36134f2e6b2b8a2610418a
Nirvana55 <lamabruno55@gmail.com>
2023-11-11 19:04:51 +0545

    refactor: update news board

--- pages/newsboard.tsx
+++ pages/newsboard.tsx
@@ -1,8 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import { NewsBoardProps } from "../types/newsBoard";
+import Head from "next/head";
+import {
+  Box,
+  Fade,
+  InputAdornment,
+  TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import WorkInProgress from "../components/WorkInProgress";
 
 const NewsBoard = (props: NewsBoardProps) => {
-  return <div>NewsBoard</div>;
+  const theme = useTheme();
+  const [searchValue, setSearchValue] = useState("");
+  const ipadScreen = useMediaQuery(theme.breakpoints.down("md"));
+
+  return (
+    <>
+      <Head>
+        <title>N | News Feed</title>
+      </Head>
+
+      <Fade in>
+        <Box sx={{ mb: 4 }}>
+          <Typography marginTop={4} variant="h4" fontWeight="bold">
+            News Feed
+          </Typography>
+        </Box>
+      </Fade>
+
+      {props.post.length > 0 ? (
+        <Fade in>
+          <Box>
+            <TextField
+              name="search"
+              type="text"
+              placeholder="Search"
+              variant="outlined"
+              value={searchValue}
+              fullWidth={ipadScreen ? true : false}
+              onChange={(e) => setSearchValue(() => e.target.value)}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end" sx={{ cursor: "pointer" }}>
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </Box>
+        </Fade>
+      ) : (
+        <WorkInProgress />
+      )}
+    </>
+  );
 };
 
 export default NewsBoard;
